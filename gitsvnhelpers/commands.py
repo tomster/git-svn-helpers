09f970cfc2435ffbf57d9d9abde95a01d34f6e10
Tom Lazar <tom@tomster.org>
2009-07-12 21:32:33 +0200

    moved the clone command into utils, since it's no longer really a command

--- gitsvnhelpers/commands.py
+++ gitsvnhelpers/commands.py
@@ -2,60 +2,12 @@ import logging
 import optparse
 import sys
 import os
-from os.path import exists
 from os.path import abspath
 from glob import glob
 from tee import popen
 from config import GIT_CACHE
-from utils import basename
-from utils import is_svn
-from utils import svn_log
-from utils import base_url
 
 logger = logging.getLogger("gitify")
-
-
-def clone():
-
-    """
-    A convenience/efficiency wrapper around git-svn clone that tries to figure
-    out the first and last revisions of the given package that need to be
-    cloned and then just clones that range.
-
-    This is particularly useful for large repositories such as plone and its
-    collective.
-    Original (bash) code from Andreas Zeidler a.k.a. witsch
-    """
-
-    if not is_svn():
-        print "This only works on svn checkouts!"
-        sys.exit(1)
-
-    package_name = basename()
-
-    if exists(GIT_CACHE + package_name):
-        print "git repository already found in %s%s." % (GIT_CACHE,
-            package_name)
-        sys.exit(1)
-
-    print "Analyzing svn log..."
-    logentries = svn_log().getiterator('logentry')
-    last_revision = logentries[0].attrib['revision']
-    first_revision = logentries[-1].attrib['revision']
-    baseurl = base_url()
-
-    print "Cloning %s from r%s:%s into %s" % (
-        baseurl, first_revision, last_revision, GIT_CACHE)
-
-    cwd = os.getcwd()
-    if not exists(GIT_CACHE):
-        os.mkdir(GIT_CACHE)
-    os.chdir(GIT_CACHE)
-    popen("git svn clone -r%s:%s %s -s" % (first_revision,
-        last_revision, baseurl))
-
-    os.chdir(cwd)
-    return 0
 
 
 class Command(object):
--- gitsvnhelpers/utils.py
+++ gitsvnhelpers/utils.py
@@ -1,6 +1,9 @@
+import sys
+import os
 from os.path import exists, isdir, islink
 from elementtree import ElementTree
 from tee import popen
+from config import GIT_CACHE
 
 
 def basename():
@@ -59,3 +62,46 @@ def svn_type():
         return 'branches'
     else:
         return 'unrecognized'
+
+def clone():
+
+    """
+    A convenience/efficiency wrapper around git-svn clone that tries to figure
+    out the first and last revisions of the given package that need to be
+    cloned and then just clones that range.
+
+    This is particularly useful for large repositories such as plone and its
+    collective.
+    Original (bash) code from Andreas Zeidler a.k.a. witsch
+    """
+
+    if not is_svn():
+        print "This only works on svn checkouts!"
+        sys.exit(1)
+
+    package_name = basename()
+
+    if exists(GIT_CACHE + package_name):
+        print "git repository already found in %s%s." % (GIT_CACHE,
+            package_name)
+        sys.exit(1)
+
+    print "Analyzing svn log..."
+    logentries = svn_log().getiterator('logentry')
+    last_revision = logentries[0].attrib['revision']
+    first_revision = logentries[-1].attrib['revision']
+    baseurl = base_url()
+
+    print "Cloning %s from r%s:%s into %s" % (
+        baseurl, first_revision, last_revision, GIT_CACHE)
+
+    cwd = os.getcwd()
+    if not exists(GIT_CACHE):
+        os.mkdir(GIT_CACHE)
+    os.chdir(GIT_CACHE)
+    popen("git svn clone -r%s:%s %s -s" % (first_revision,
+        last_revision, baseurl))
+
+    os.chdir(cwd)
+    return 0
+
