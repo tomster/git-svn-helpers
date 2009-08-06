6678a00ca17099a0f2ed928b2c0c3ac738ad0c24
Tom Lazar <tom@tomster.org>
2009-08-06 12:58:20 +0100

    Created a test case for commands that captures stdout and stderr in order to allow to make assertions regarding their output.

--- gitsvnhelpers/testing.py
+++ gitsvnhelpers/testing.py
@@ -1,7 +1,10 @@
 import shutil
+import sys
+import StringIO
 from os.path import join, dirname
 from jarn.mkrelease.testing import SubversionSetup, JailSetup
 from jarn.mkrelease.process import Process
+
 
 class BaseTestCase(SubversionSetup):
 
@@ -23,3 +26,37 @@ class BaseTestCase(SubversionSetup):
         process.system('svn checkout file://%s/%s %s' % (self.packagedir,
             path, self.source))
         self.checkoutdir = join(self.tempdir, self.source)
+
+
+class StdOut(StringIO.StringIO):
+    """ A StringIO based stdout replacement that optionally mirrors all
+        output to stdout in addition to capturing it.
+    """
+
+    def __init__(self, stdout):
+        self.__stdout = stdout
+        StringIO.StringIO.__init__(self)
+
+    def write(self, s):
+        # uncomment the following for debugging tests!
+        # #self.__stdout.write(s)
+        StringIO.StringIO.write(self, s)
+
+    def read(self):
+        self.seek(0)
+        self.__stdout.write(StringIO.StringIO.read(self))
+
+
+class CommandTestCase(BaseTestCase):
+    """a test class that captures stdout and stderr"""
+
+    def setUp(self):
+        BaseTestCase.setUp(self)
+        self.out = StdOut(sys.stdout)
+        self.err = StdOut(sys.stdout)
+        sys.stdout = self.out
+        sys.stderr = self.err
+
+    def cleanUp(self):
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
