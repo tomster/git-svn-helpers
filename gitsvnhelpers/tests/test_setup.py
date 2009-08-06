25b2acb5f06eef2cab77a882a83caa63a2b26c8e
Tom Lazar <tom@tomster.org>
2009-08-06 09:10:23 +0100

    Created a tag in the test svn repository and test the utilities against that, as well.

--- gitsvnhelpers/tests/test_setup.py
+++ gitsvnhelpers/tests/test_setup.py
@@ -33,3 +33,14 @@ class TestUtils(BaseTestCase):
         self.failUnlessEqual(svn_branch(), 'feature-bar')
         self.failUnless(svn_url().endswith('/testpackage/branches/feature-bar'))
         self.failUnless(base_url().endswith('/testpackage/'))
+
+    def test_utils_on_tag(self):
+        self.checkout('tags/0.1')
+        os.chdir(self.checkoutdir)
+        self.failUnless(is_svn())
+        self.failIf(is_git())
+        self.failUnlessEqual(svn_type(), 'tags')
+        self.failUnlessEqual(basename(), 'testrepo.svn')
+        self.failUnlessEqual(svn_branch(), '0.1')
+        self.failUnless(svn_url().endswith('/testpackage/tags/0.1'))
+        self.failUnless(base_url().endswith('/testpackage/'))
