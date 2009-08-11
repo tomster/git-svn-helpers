4dc09f63cc692a8f22c042b7fda27b0693e9f9fb
Tom Lazar <tom@tomster.org>
2009-08-11 13:12:40 +0200

    A convenience method to run shell commands in doctests

--- gitsvnhelpers/tests/test_doctests.py
+++ gitsvnhelpers/tests/test_doctests.py
@@ -1,4 +1,5 @@
 import doctest
+from jarn.mkrelease.tee import popen
 from gitsvnhelpers.testing import BaseTestCase
 
 optionflags = (doctest.REPORT_ONLY_FIRST_FAILURE |
@@ -8,9 +9,15 @@ optionflags = (doctest.REPORT_ONLY_FIRST_FAILURE |
 
 class TestDoctests(BaseTestCase):
 
+    def shell(self, cmd):
+        """executes the shell command and prints its output"""
+        code, output = popen(cmd, False, False)
+        for line in output:
+            print line
+
     def test_doctests(self):
         for testfile in ['test_gitify.txt',]:
             doctest.testfile(testfile,
-                globs=dict(self=self),
+                globs=dict(self=self, do=self.shell),
                 report=True,
                 optionflags=optionflags)
