tomster/git-svn-helpers

commit 1930852cecad84341a5d1d0c98bd462565bdaac7

use python's own (preferably C) implementation of elementtree

gitsvnhelpers/utils.py
@@ -1,7 +1,11 @@
 import sys
 import os
 from os.path import exists, isdir, islink
-from elementtree import ElementTree
+try:
+    from xml.etree import cElementTree as ElementTree
+except ImportError:
+    from xml.etree import ElementTree
+
 from jarn.mkrelease.tee import popen
 import config
 
@@ -25,15 +29,17 @@ def is_svn():
 def svn_info():
     """Returns the svn info as XML element"""
     code, result = popen('svn info --xml .', False, False)
-    return ElementTree.fromstring(''.join(result))
-
+    parser = ElementTree.XMLTreeBuilder()
+    parser.feed(''.join(result))
+    return parser.close()
 
 def svn_log():
     """Returns the svn log of the base url as XML element"""
     code, result = popen('svn log --stop-on-copy --xml %s' % base_url(),
         False, False)
-    return ElementTree.fromstring(''.join(result))
-
+    parser = ElementTree.XMLTreeBuilder()
+    parser.feed(''.join(result))
+    return parser.close()
 
 def svn_url(svninfo=None):
     """ returns the URL of the svn checkout"""

setup.py
@@ -36,7 +36,6 @@ setup(name='git-svn-helpers',
     test_suite='gitsvnhelpers.tests',
     install_requires=[
         # -*- Extra requirements: -*-
-        "elementtree",
         "jarn.mkrelease",
     ],
     entry_points="""

commit 6abc33cd34921361db1d0f8fbbe786790f2764ed

enforce a list, as cElementTree returns a generator. Fixes bug introduced in [1930852cecad84341a5d1d0c98bd462565bdaac7]

gitsvnhelpers/utils.py
@@ -93,7 +93,7 @@ def clone():
         sys.exit(1)
 
     print "Analyzing svn log..."
-    logentries = svn_log().getiterator('logentry')
+    logentries = list(svn_log().getiterator('logentry'))
     last_revision = logentries[0].attrib['revision']
     first_revision = logentries[-1].attrib['revision']
     baseurl = base_url()